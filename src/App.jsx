1bd589f06e5865307e666e037b7b399e51b44807
CepotOi <rizkihutama69@gmail.com>
2022-11-04 21:19:05 +0700

    Handling Http Errors

--- src/App.jsx
+++ src/App.jsx
@@ -6,34 +6,52 @@ import './App.css';
 const App = () => {
   const [movies, setMovies] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   const fetchMoviesHandler = async () => {
     setIsLoading(true);
-    const res = await fetch('https://swapi.dev/api/films/');
-    const data = await res.json();
-    const transformedData = data.results.map(movieData => {
-      return {
-        id: movieData.episode_id,
-        title: movieData.title,
-        openingText: movieData.opening_crawl,
-        releaseDate: movieData.release_date,
-      };
-    });
+    setError(null);
 
-    setMovies(transformedData);
-    setIsLoading(false);
+    try {
+      const res = await fetch('https://swapi.dev/api/films/');
+      if (!res.ok) throw new Error('Something went wrong!');
+      const data = await res.json();
+      const transformedData = data.results.map(movieData => {
+        return {
+          id: movieData.episode_id,
+          title: movieData.title,
+          openingText: movieData.opening_crawl,
+          releaseDate: movieData.release_date,
+        };
+      });
+      setMovies(transformedData);
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setIsLoading(false);
+    }
   };
+
+  let content = <p>No Movies Found</p>;
+
+  if (movies.length > 0) {
+    content = <MoviesList movies={movies} />;
+  }
+
+  if (isLoading) {
+    content = <p>Loading...</p>;
+  }
+
+  if (error) {
+    content = <p>Something went wrong!</p>;
+  }
 
   return (
     <>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
-      <section>
-        {!isLoading && movies.length > 0 && <MoviesList movies={movies} />}
-        {!isLoading && movies.length === 0 && <p>No Movies Found</p>}
-        {isLoading && <p>Loading...</p>}
-      </section>
+      <section>{content}</section>
     </>
   );
 };
